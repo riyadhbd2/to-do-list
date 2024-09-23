757acd8b234e4f55ca48ebc70d6f3b0852462743
riyadhbd2 <riyadhbd2@gmail.com>
2024-09-23 07:30:35 +0200

    delete function added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -24,6 +24,12 @@ const Todo = () => {
         inputRef.current.value = "";
     }
 
+    const deleteTodo = (id) =>{
+        setTodoList((prvTodos)=>{
+           return prvTodos.filter((todo)=> todo.id !== id)
+        })
+    }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
         
@@ -46,7 +52,7 @@ const Todo = () => {
     <div>
         {
             todoList.map((item, index)=>{
-                return <TodoItems key={index} text={item.text}></TodoItems>
+                return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}></TodoItems>
             })
         }
     
